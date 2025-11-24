08c2277ee1f04bffd68dee62efd567dcf6df2af0
LDagnachew <leulmdagnachew@gmail.com>
2025-11-24 13:39:16 -0600

    Hash Indexing V1: - Implemented a barebones Hash Index, naive Put/Get Methods Complete

--- Hash-Indexes/hashindex.go
+++ Hash-Indexes/hashindex.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"encoding/binary"
 	"errors"
 	"os"
 )
@@ -23,13 +24,23 @@ func NewHashIndex() (*HashIndex, error) {
 }
 
 func (h *HashIndex) Put(key, value []byte) error {
-	// TODO: Simply Append to Logfile
-	lastByte, err := h.f.Seek(-1, 2)
+	lastByte, err := h.f.Seek(0, 2)
 	if err != nil {
 		return err
 	}
+	// KEY_LEN, VAL_LEN
+	keylen32 := make([]byte, 4)
+	vallen32 := make([]byte, 4)
+	// place using little endian
+	binary.BigEndian.PutUint32(keylen32, uint32(len(key)))
+	binary.BigEndian.PutUint32(vallen32, uint32(len(value)))
+	// write these to dataseg
 	h.index[string(key)] = lastByte
-	h.f.WriteAt(value, lastByte)
+	h.f.WriteAt(keylen32, lastByte)
+	h.f.WriteAt(vallen32, lastByte+4)
+	// now write KV pair
+	h.f.WriteAt(key, lastByte+8)
+	h.f.WriteAt(value, lastByte+int64(8)+int64(len(key)))
 	return nil
 }
 
@@ -40,9 +51,17 @@ func (h *HashIndex) Get(key []byte) ([]byte, error) {
 		return []byte{}, errors.New("This Key Doesn't Exist")
 	}
 	// Seek, then read
-}
+	// Skip Key, Read value len, go to position lastByte + 8 + key_len, read value & return
+	keylen32 := make([]byte, 4)
+	vallen32 := make([]byte, 4)
 
-func (h *HashIndex) Delete(key []byte) error {
+	h.f.ReadAt(keylen32, off)
+	h.f.ReadAt(vallen32, off+4)
+	// turn to ints
+	k_len := binary.BigEndian.Uint32(keylen32)
 
-	return nil
+	// now read the value + return
+	res := make([]byte, int(k_len))
+	h.f.ReadAt(res, off+8+int64(k_len))
+	return res, nil
 }
