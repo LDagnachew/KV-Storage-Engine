8f027decdf56d14871d44decea83049a535c00f3
LDagnachew <leulmdagnachew@gmail.com>
2025-11-28 15:32:21 -0600

    Concurrency Handling for Hash Indexes

--- hash_indexes/hashindex.go
+++ hash_indexes/hashindex.go
@@ -7,6 +7,7 @@ import (
 	"io"
 	"log"
 	"os"
+	"sync"
 )
 
 /*
@@ -22,6 +23,15 @@ V2 Design:
 Fill Segment File before moving onto the next one
 Preparation for Compaction
 */
+
+/*
+V3 Design (Hard Part)
+Implement Compaction + Merging
+The Idea is that we want to perform this compaction/merging asynchronously
+- P0: We must now make our HashMap thread safe. Eventually we will have to modify the byte offsets when we merge.
+	  Therefore, if one thread is updating while another thread is merging, we need to avoid this race condition.
+*/
+
 type SegmentFile struct {
 	f    *os.File
 	size int64
@@ -32,10 +42,11 @@ type Entry struct {
 	byte_offset int64
 }
 type HashIndex struct {
-	f        *os.File         // TODO get rid of this LOL
-	segments []*SegmentFile   // Array of Current Segment Files
-	index    map[string]Entry // key -> offset in log
-	maxSize  int64            // maximum size of a segment file
+	log_mutex sync.Mutex // Single Writer to log files
+	idx_mutex sync.RWMutex
+	segments  []*SegmentFile   // Array of Current Segment Files
+	index     map[string]Entry // key -> offset in log
+	maxSize   int64            // maximum size of a segment file
 }
 
 func NewHashIndex() (*HashIndex, error) {
@@ -45,7 +56,6 @@ func NewHashIndex() (*HashIndex, error) {
 	}
 	seg := []*SegmentFile{{f: file, size: 0}}
 	return &HashIndex{
-		f:        file,
 		segments: seg,
 		index:    make(map[string]Entry),
 		maxSize:  32 * 1024, // for now
@@ -53,17 +63,21 @@ func NewHashIndex() (*HashIndex, error) {
 }
 
 func (h *HashIndex) Put(key, value []byte) error {
-	current_segment := h.segments[len(h.segments)-1]
-	file := current_segment.f
-	lastByte, err := file.Seek(0, io.SeekEnd)
-	if err != nil {
-		return err
-	}
-	// KEY_LEN, VAL_LEN
+
+	total_entry_size := 8 + int64(len(key)) + int64(len(value))
 	keylen32 := make([]byte, 4)
 	vallen32 := make([]byte, 4)
 
-	total_entry_size := 8 + int64(len(key)) + int64(len(value))
+	h.log_mutex.Lock()
+	// KEY_LEN, VAL_LEN
+	current_segment := h.segments[len(h.segments)-1]
+	file := current_segment.f
+
+	lastByte, err := file.Seek(0, io.SeekEnd)
+	if err != nil {
+		h.log_mutex.Unlock()
+		return err
+	}
 	// Special Case: If we are going to be writing what will exceed HashIndex Limit make new segment
 	if current_segment.size+total_entry_size > h.maxSize {
 		h.createDataSegment()
@@ -78,20 +92,29 @@ func (h *HashIndex) Put(key, value []byte) error {
 	binary.BigEndian.PutUint32(keylen32, uint32(len(key)))
 	binary.BigEndian.PutUint32(vallen32, uint32(len(value)))
 	// write these to dataseg
-	h.index[string(key)] = Entry{f: file, byte_offset: lastByte}
+
 	log.Printf("lastByte currently is %d\n", lastByte)
+
 	file.WriteAt(keylen32, lastByte)
 	file.WriteAt(vallen32, lastByte+4)
 	// now write KV pair
 	file.WriteAt(key, lastByte+8)
 	file.WriteAt(value, lastByte+int64(8)+int64(len(key)))
 	file.Sync()
+	h.log_mutex.Unlock()
+
+	h.idx_mutex.Lock()
+	h.index[string(key)] = Entry{f: file, byte_offset: lastByte}
+	current_segment.size += total_entry_size
+	h.idx_mutex.Unlock()
 	return nil
 }
 
 func (h *HashIndex) Get(key []byte) ([]byte, error) {
 	// TODO: Fetch from map, read from byte offset
+	h.idx_mutex.Lock()
 	entry, ok := h.index[string(key)]
+	h.idx_mutex.Unlock()
 	off := entry.byte_offset
 	file := entry.f
 	if !ok {
@@ -116,7 +139,22 @@ func (h *HashIndex) Get(key []byte) ([]byte, error) {
 }
 
 func (h *HashIndex) Close() error {
-	return h.f.Close()
+	h.log_mutex.Lock()
+	defer h.log_mutex.Unlock()
+
+	var firstErr error
+	for _, seg := range h.segments {
+		if seg == nil || seg.f == nil {
+			continue
+		}
+		if err := seg.f.Close(); err != nil && firstErr == nil {
+			firstErr = err
+		}
+		seg.f = nil
+	}
+	h.segments = nil
+	h.index = nil
+	return firstErr
 }
 
 func (h *HashIndex) createDataSegment() error {
